e1f9bebd74fa0ec981aae8fcaad1eca7b0e64d0f
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-04-12 18:27:32 +0800

    added isleapyear and test

--- daysbetweendates.py
+++ daysbetweendates.py
@@ -4,62 +4,40 @@
 # Simply put, if you were born 1 Jan 2012 and todays date is 
 # 2 Jan 2012 you are 1 day old.
 
-# IMPORTANT: You don't need to solve the problem yet! 
-# Just brainstorm ways you might approach it!
-
-# daysOfMonths = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
-# def isLeapYear(year):
-#     ##
-#     # Your code here. Return True or False
-#     # Pseudo code for this algorithm is found at
-#     # http://en.wikipedia.org/wiki/Leap_year#Algorithm
-#     ##
-
-# def daysBetweenDates(y1, m1, d1, y2, m2, d2):
-#     ##
-#     # Your code here.
-#     ##
-#     return days
-
-
-
-    ###
-### Define a simple nextDay procedure, that assumes
-### every month has 30 days.
-###
-### For example:
-###    nextDay(1999, 12, 30) => (2000, 1, 1)
-###    nextDay(2013, 1, 30) => (2013, 2, 1)
-###    nextDay(2012, 12, 30) => (2013, 1, 1)  (even though December really has 31 days)
-###
-
-# def nextDay(year, month, day):
-#     """
-#     Returns the year, month, day of the next day.
-#     Simple version: assume every month has 30 days.
-#     """
-#     # YOUR CODE HERE
-#     if day < 30:
-#         return year, month, day + 1
-#     else:
-#         if month < 12:
-#             return year, month + 1, 1
-#         else:
-#             return year + 1, 1, 1
-
-
-# Define a daysBetweenDates procedure that would produce the
-# correct output if there was a correct nextDay procedure.
+# Credit goes to Websten from forums
 #
-# Note that this will NOT produce correct outputs yet, since
-# our nextDay procedure assumes all months have 30 days
-# (hence a year is 360 days, instead of 365).
-# 
+# Use Dave's suggestions to finish your daysBetweenDates
+# procedure. It will need to take into account leap years
+# in addition to the correct number of days in each month.
+
+def isLeapYear(year):
+    if year % 400 == 0:
+        return True
+    if year % 100 == 0:
+        return False
+    if year % 4 == 0:
+        return True
+    return False
+
+
+def daysInMonth(year, month):
+    if month == 1 or month == 3 or month == 5 or month == 7 \
+        or month == 8 or month==10 or month == 12:
+        return 31
+    else : 
+        if month == 2:
+            if isLeapYear(year):
+                return 29
+            else:
+                return 28
+        else:
+            return 30
+    return 30
+
 
 def nextDay(year, month, day):
     """Simple version: assume every month has 30 days"""
-    if day < 30:
+    if day < daysInMonth(year, month):
         return year, month, day + 1
     else:
         if month == 12:
@@ -67,37 +45,35 @@ def nextDay(year, month, day):
         else:
             return year, month + 1, 1
         
-def daysBetweenDates(year1, month1, day1, year2, month2, day2):
-    """Returns the number of days between year1/month1/day1
-       and year2/month2/day2. Assumes inputs are valid dates
-       in Gregorian calendar, and the first date is not after
-       the second."""
-    
-    assert not dateisbefore(year2, month2, day2,year1, month1, day1)
-    # YOUR CODE HERE!
-    days = 0
-    while dateisbefore(year1, month1, day1, year2, month2, day2):
-        year1,month1, day1 = nextDay(year1, month1, day1)
-        days +=1
-    return days
-
-def dateisbefore(year1, month1, day1, year2, month2, day2):
-   
+def dateIsBefore(year1, month1, day1, year2, month2, day2):
+    """Returns True if year1-month1-day1 is before year2-month2-day2. Otherwise, returns False."""
     if year1 < year2:
         return True
-    
     if year1 == year2:
         if month1 < month2:
             return True
         if month1 == month2:
-            return day1<day2
-    return False
+            return day1 < day2
+    return False        
 
+def daysBetweenDates(year1, month1, day1, year2, month2, day2):
+    """Returns the number of days between year1/month1/day1
+       and year2/month2/day2. Assumes inputs are valid dates
+       in Gregorian calendar."""
+    # program defensively! Add an assertion if the input is not valid!
+    assert not dateIsBefore(year2, month2, day2, year1, month1, day1)
+    days = 0
+    while dateIsBefore(year1, month1, day1, year2, month2, day2):
+        year1, month1, day1 = nextDay(year1, month1, day1)
+        days += 1
+    return days
 
 def test():
-    test_cases = [((2012,9,30,2012,10,30),30), 
-                  ((2012,1,1,2013,1,1),360),
-                  ((2012,9,1,2012,9,4),3)]
+    test_cases = [((2012,1,1,2012,2,28), 58), 
+                  ((2012,1,1,2012,3,1), 60),
+                  ((2011,6,30,2012,6,30), 366),
+                  ((2011,1,1,2012,8,8), 585 ),
+                  ((1900,1,1,1999,12,31), 36523)]
     
     for (args, answer) in test_cases:
         result = daysBetweenDates(*args)
@@ -105,15 +81,32 @@ def test():
             print "Test with data:", args, "failed"
         else:
             print "Test case passed!"
-        except AssertionError:
-            if answer == "AssertionError":
-                print "Nice job! Test case {0} correctly raises AssertionError!\n".format(args)
-            else:
-                print "Check your work! Test case {0} should not raise AssertionError!\n".format(args)
 
-#test()
+def mytest():
+    assert daysBetweenDates(2013,1,1,2013,1,1) == 0
+    assert daysBetweenDates(2013,1,1,2013,1,2) == 1
+    assert nextDay(2013,1,1) == (2013,1,2)
+    assert nextDay(2013,4,30) == (2013,5,1)
+    assert nextDay(2012,12,31) == (2013,1,1)
+    assert nextDay(2013,2,28) == (2013,3,1)
+    assert nextDay(2013,9,30) == (2013,10,1)
+    assert nextDay(2012,2,28) == (2012,2,29)
+    assert daysBetweenDates(2012,1,1,2013,1,1) == 366
+    assert daysBetweenDates(2013,1,1,2014,1,1) == 365
+    assert daysBetweenDates(2013,1,24,2013,6,29) == 156
+    print "Test Finished!"
 
-print daysBetweenDates(2013,1,24,2013,6,29)
+test()
+    # tests with 30-days months
+   
+# Pythonistas Guide to all problems in the galaxy
+# 0. Dont Panic
+# 1. What are the inputs
+# 2. What are the outputs
+# 3. Worth Through some examples by hand
+# 4. Simple mechanical solution
+# 5. develop incrementally and test as we go
+
 
 
 
